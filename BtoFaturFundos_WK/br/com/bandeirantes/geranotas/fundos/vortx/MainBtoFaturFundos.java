aec27eb7e024d5ecc43d7b28468e3400bf6a0e5f
Seu Nome Pessoal <gustavossantos41@outlook.com>
2026-03-20 13:59:41 -0300

    Ultima versão antes do claude

--- BtoFaturFundos_WK/br/com/bandeirantes/geranotas/fundos/vortx/MainBtoFaturFundos.java
+++ BtoFaturFundos_WK/br/com/bandeirantes/geranotas/fundos/vortx/MainBtoFaturFundos.java
@@ -2,10 +2,24 @@ package br.com.bandeirantes.geranotas.fundos.vortx;
 
 import br.com.sankhya.extensions.actionbutton.AcaoRotinaJava;
 import br.com.sankhya.extensions.actionbutton.ContextoAcao;
+import br.com.sankhya.extensions.actionbutton.Registro;
+import br.com.sankhya.jape.wrapper.JapeFactory;
+import br.com.sankhya.jape.wrapper.JapeWrapper;
+import br.com.sankhya.modelcore.util.DynamicEntityNames;
+
+import java.math.BigDecimal;
 
 public class MainBtoFaturFundos implements AcaoRotinaJava {
-    @Override
-    public void doAction(ContextoAcao contextoAcao) throws Exception {
 
+    static JapeWrapper usuDAO = JapeFactory.dao(DynamicEntityNames.USUARIO);
+
+    @Override
+    public void doAction(ContextoAcao ctx) throws Exception {
+        Registro[] linhas = ctx.getLinhas();
+        BigDecimal codUsu = usuDAO.findByPK(ctx.getUsuarioLogado()).asBigDecimal("CODUSU");
+
+        for (Registro linha : linhas) {
+            //validaNotas(linha);
+        }
     }
 }
